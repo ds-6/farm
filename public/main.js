0594c8861b2b20ba78f629645fdf66f33b3bf537
deepak <deepaksoni06@gmail.com>
2020-07-12 15:18:47 +0530

    profile-div added

--- public/main.js
+++ public/main.js
@@ -12,3 +12,11 @@ _fn('.menu-bars').addEventListener('click',(e)=>{
 _fn('.cross').addEventListener('click',(e)=>{
     _fn('.nav-menu').classList.remove('show');
 })
+
+_fn('.profile').addEventListener('click',(e)=>{
+    _fn('.profile-div').classList.add('show');
+    _fn('.nav-menu').classList.remove('show');
+})
+_fn('.profile-div .crossBTN').addEventListener('click',(e)=>{
+    _fn('.profile-div').classList.remove('show');
+})
